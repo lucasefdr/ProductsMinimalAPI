4e60fd5ab13479dd2bc3e000c034c298a48d2c88
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-11-16 17:57:10 -0300

    feat: add DbContext

---
+++ ProductsMinimalAPI/Context/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsMinimalAPI.Models;
+
+namespace ProductsMinimalAPI.Context;
+
+public class AppDbContext(DbContextOptions opts) : DbContext(opts)
+{
+    DbSet<Category>? Categories { get; set; }
+    DbSet<Product>? Products { get; set; }
+}
