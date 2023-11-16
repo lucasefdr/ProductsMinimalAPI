89d26def93b83485378f00b14bb8730933068bac
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-11-16 18:08:51 -0300

    feat: add configuration with Fluent API

--- ProductsMinimalAPI/Context/AppDbContext.cs
+++ ProductsMinimalAPI/Context/AppDbContext.cs
@@ -7,4 +7,27 @@ public class AppDbContext(DbContextOptions opts) : DbContext(opts)
 {
     DbSet<Category>? Categories { get; set; }
     DbSet<Product>? Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Category
+        modelBuilder.Entity<Category>().HasKey(c => c.CategoryId);
+        modelBuilder.Entity<Category>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+        modelBuilder.Entity<Category>().Property(c => c.Description).HasMaxLength(150).IsRequired();
+
+        // Product
+        modelBuilder.Entity<Product>().HasKey(p => p.ProductId);
+        modelBuilder.Entity<Product>().Property(p => p.Name).HasMaxLength(100).IsRequired();
+        modelBuilder.Entity<Product>().Property(p => p.Description).HasMaxLength(150).IsRequired();
+        modelBuilder.Entity<Product>().Property(p => p.ImageUrl).HasMaxLength(150);
+        modelBuilder.Entity<Product>().Property(p => p.Price).HasPrecision(14, 2);
+
+        // Relationship
+        modelBuilder.Entity<Product>().HasOne<Category>(p => p.Category)
+            .WithMany(c => c.Products)
+            .HasForeignKey(p => p.CategoryId);
+
+
+
+    }
 }
