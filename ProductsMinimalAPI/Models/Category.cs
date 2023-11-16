0d0ffe862982f00083bc6e6d6335f7e9236b5459
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-11-16 17:53:52 -0300

    feat: add models Product and Category

---
+++ ProductsMinimalAPI/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductsMinimalAPI.Models;
+
+[Table("category")]
+public class Category
+{
+    [Column("categoryid")]
+    public int CategoryId { get; set; }
+
+    [Column("name")]
+    public string? Name { get; set; }
+
+    [Column("description")]
+    public string? Description { get; set; }
+    
+    // N => 1 
+    public ICollection<Product>? Products { get; set; }
+}
---
+++ ProductsMinimalAPI/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductsMinimalAPI.Models;
+
+[Table("product")]
+public class Product
+{
+    [Column("productid")]
+    public int ProductId { get; set; }
+
+    [Column("name")]
+    public string? Name { get; set; }
+
+    [Column("description")]
+    public string? Description { get; set; }
+
+    [Column("price")]
+    public decimal Price { get; set; }
+
+    [Column("imageurl")]
+    public string ImageUrl { get; set; }
+
+    [Column("purchasedate")]
+    public DateTime PurchaseDate { get; set; }
+
+    [Column("stock")]
+    public int Stock { get; set; }
+
+
+    [Column("categoryid")]
+    public int CategoryId { get; set; }
+
+    public Category? Category { get; set; }
+}
